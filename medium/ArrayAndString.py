36188c16244dee89d02408ffd5b7da1f4711a2d1
olin <olin6520@gmail.com>
2018-06-25 15:18:31 +0800

    完成Longest Palindromic Substring

--- medium/ArrayAndString.py
+++ medium/ArrayAndString.py
@@ -1,54 +1,60 @@
-def lengthOfLongestSubstring(s):
-    max_len = 0
-    max_str = ""
-    i = 0
-    j = 1
-    str_len = len(s)
-    if str_len == 1 or str_len == 0:
-        return str_len
-    else:
-        my_set = set(s[i])
-        while j < str_len:
-            before_len = len(my_set)
-            my_set.add(s[j])
-            if before_len == len(my_set):
-                if j - i > max_len:
-                    max_len = j - i
-                    max_str = s[i:j]
-                my_set.clear()
-                for x in range(i, j):
-                    if s[x] == s[j]:
-                        i = x + 1
-                        break
-                my_set = set(s[i:j])
-            else:
-                if j - i + 1 > max_len:
-                    max_len = j - i + 1
-                    max_str = s[i:j + 1]
-                j += 1
-    if i == 0:
-        return s
-    else:
-        return max_str
-
-
-def longestPalindrome(s):
-    longest_palindrome = ""
-    longest_length = 0
-    length = len(s)
-    reverse_s = s[::-1]
-    i, j = 0, 0
-    while i < length:
-        temp_i = i
-        j = 0
-        while j < length:
-            if s[temp_i] != reverse_s[j]:
-                j += 1
-            else:
-                while temp_i < length and j < length and s[temp_i] == reverse_s[j]:
-                    temp_i += 1
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        max_len = 0
+        max_str = ""
+        i = 0
+        j = 1
+        str_len = len(s)
+        if str_len == 1 or str_len == 0:
+            return str_len
+        else:
+            my_set = set(s[i])
+            while j < str_len:
+                before_len = len(my_set)
+                my_set.add(s[j])
+                if before_len == len(my_set):
+                    if j - i > max_len:
+                        max_len = j - i
+                        max_str = s[i:j]
+                    my_set.clear()
+                    for x in range(i, j):
+                        if s[x] == s[j]:
+                            i = x + 1
+                            break
+                    my_set = set(s[i:j])
+                else:
+                    if j - i + 1 > max_len:
+                        max_len = j - i + 1
+                        max_str = s[i:j + 1]
                     j += 1
+        if i == 0:
+            return s
+        else:
+            return max_str
+
+    # 更好的算法是马拉车算法
+    def longestPalindrome(self, s):
+        longest_str = ""
+
+        length = len(s)
+        if length <= 1:
+            return s
+        for i in range(length):
+            temp1 = self.helper(s, i, i)
+            temp2 = self.helper(s, i, i + 1)
+            if len(temp1) > len(temp2) and len(temp1) > len(longest_str):
+                longest_str = temp1
+            if len(temp1) < len(temp2) and len(temp2) > len(longest_str):
+                longest_str = temp2
+        return longest_str
+
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l + 1:r]
 
 
 if __name__ == "__main__":
-    print(longestPalindrome("aab"))
+    print(Solution().longestPalindrome(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
